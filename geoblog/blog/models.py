52d1fb08bf95f0ddfab471133930ea5501c0575c
ilyas <ilya.surgutskij11@mail.ru>
2024-11-15 15:16:28 +0500

    commit for commit

--- geoblog/blog/models.py
+++ geoblog/blog/models.py
@@ -9,7 +9,7 @@ class post(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     date = models.TimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
 
     def __str__(self):
         return self.title
